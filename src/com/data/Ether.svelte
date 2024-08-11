d88267e8e47dec72629aed97b204d4b7ca48b6cf
Craig Hopgood <searaig@gmail.com>
2024-08-11 11:29:48 -0400

    add formatted ether com

---
+++ src/com/data/Ether.svelte
@@ -0,0 +1,14 @@
+<script>
+import { ethers } from "ethers";
+
+export let wei
+const formatter = new Intl.NumberFormat( 'en-CA', { minimumFractionDigits: 4 } )
+$:ether = formatter.format(
+    ethers.formatUnits( wei, 'ether' )
+)
+</script>
+
+<div class="flex items-center">
+    <span>{ ether }</span>
+    <span class="icon-[token--eth]"></span>
+</div>
