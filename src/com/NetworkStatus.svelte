a6aec20097f6ab5b6bf8f8539cee632cba29f5d8
Craig Hopgood <searaig@gmail.com>
2024-08-11 15:34:28 -0400

    spacing for network status

--- src/com/NetworkStatus.svelte
+++ src/com/NetworkStatus.svelte
@@ -1,8 +1,7 @@
 <script>
 export let network
 </script>
-<div data-tid="network-status" data-network-status={network.status} class="flex items-center gap-1">
-    <div class="text-3xl">{network.name}</div>
+<div data-tid="network-status" data-network-status={network.status} class="flex items-center gap-2">
     <input
         type="radio"
         class="radio"
@@ -10,4 +9,5 @@ export let network
         class:radio-error={!network.available}
         class:radio-success={network.available}
     />
+    <div class="text-3xl">{network.name}</div>
 </div>
