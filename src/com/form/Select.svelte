90c97724ebe54186fde59ec919226af0cc9774aa
Craig Hopgood <searaig@gmail.com>
2024-08-11 15:33:57 -0400

    select com

---
+++ src/com/form/Select.svelte
@@ -0,0 +1,23 @@
+<script>
+export let options
+export let title
+export let selected
+</script>
+
+<label class="form-control w-full">
+    <select class="select select-bordered" on:change>
+        <option  selected={selected}>Select ...</option>
+
+        {#each options as option}
+            <option
+                value={option.value}
+                selected={selected==option.value}
+            >{option.label}</option>
+        {/each}
+    </select>
+
+    <div class="label">
+        <span class="label-text-alt">{title}</span>
+    </div>
+
+</label>
