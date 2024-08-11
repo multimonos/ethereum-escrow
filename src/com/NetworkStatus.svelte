629fd083478cff48a8b7073eb1f2dac4ad066b73
Craig Hopgood <searaig@gmail.com>
2024-08-11 09:55:43 -0400

    add testable data-network-status attr

--- src/com/NetworkStatus.svelte
+++ src/com/NetworkStatus.svelte
@@ -26,7 +26,7 @@ onMount( async () => {
 
 } )
 </script>
-<div data-tid="network-status" class="flex items-center gap-1">
+<div data-tid="network-status" data-network-status={network.status} class="flex items-center gap-1">
     <div class="text-3xl">{network.name}</div>
     <input
         type="radio"
