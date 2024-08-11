2be85c059482e119fe4bb77391052b70df3704d3
Craig Hopgood <searaig@gmail.com>
2024-08-11 11:30:31 -0400

    list accounts with balance

---
+++ src/com/Accounts.svelte
@@ -0,0 +1,30 @@
+<script>
+// list accounts and their balances
+import Panel from "./card/Panel.svelte";
+import Ether from "./data/Ether.svelte";
+//props
+export let accounts = []
+</script>
+
+<Panel title="accounts">
+    <table class="table table-sm font-mono mt-2">
+        <thead>
+            <tr>
+                <th></th>
+                <th>Balance</th>
+                <th>Address</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each accounts as account,i}
+                <tr data-address={account.address}>
+                    <td>{i}</td>
+                    <td>
+                        <Ether wei={account.balance}/>
+                    </td>
+                    <td>{account.address}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</Panel>
