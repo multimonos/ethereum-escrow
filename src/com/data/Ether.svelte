41a9ceb797649209d931151d2dbf5edffb155e72
Craig Hopgood <searaig@gmail.com>
2024-08-13 14:46:02 -0400

    colorize option to Ether

--- src/com/data/Ether.svelte
+++ src/com/data/Ether.svelte
@@ -2,13 +2,19 @@
 import { ethers } from "ethers";
 
 export let wei
+export let colorize = false
+
 const formatter = new Intl.NumberFormat( 'en-CA', { minimumFractionDigits: 4 } )
+
 $:ether = formatter.format(
     ethers.formatUnits( wei, 'ether' )
 )
 </script>
-
-<div class="flex items-center gap-x-1">
+<div class="flex items-center gap-x-1 {
+     colorize && parseInt(wei) > 0 ? 'text-success'
+     : colorize && parseInt(wei) <= 0 ? 'text-error'
+     : ''
+     }">
     <span class="text-base-content icon-[token--eth]"></span>
-    <span >{ ether }</span>
+    <span>{ ether }</span>
 </div>
