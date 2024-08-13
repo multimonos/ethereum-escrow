5d4d387d7e9271522bb7a825db13d69c79cdd01f
Craig Hopgood <searaig@gmail.com>
2024-08-13 15:17:34 -0400

    page cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,25 @@
 <script>
 import { useMachine } from "@xstate/svelte";
 import { setup } from "xstate";
+import { onMount } from "svelte";
+import { modelOptionsForSelect } from "$lib/form.js";
 import { escrowMachine } from "$lib/machine/escrow-machine.js";
 import { networkId } from "$lib/config.js";
-import Error from "../com/Error.svelte";
+import { abi, bytecode } from "../sol/Escrow.json"
+import AccountStatus from "../com/AccountStatus.svelte";
 import Accounts from "../com/Accounts.svelte";
+import Card from "../com/card/Card.svelte";
+import Deployments from "../com/Deployments.svelte";
+import Error from "../com/Error.svelte";
+import FormRow from "../com/form/FormRow.svelte";
+import Label from "../com/form/Label.svelte";
 import NetworkStatus from "../com/NetworkStatus.svelte";
-import { modelOptionsForSelect } from "$lib/form.js";
 import Select from "../com/form/Select.svelte";
 import StackedLayout from "../com/card/StackedLayout.svelte";
-import { abi, bytecode } from "../sol/Escrow.json"
-import WalletStatus from "../com/WalletStatus.svelte";
-import AccountStatus from "../com/AccountStatus.svelte";
-import { onMount } from "svelte";
-import Toasts from "../com/Toasts.svelte";
 import Steps from "../com/Steps.svelte";
-import Card from "../com/card/Card.svelte";
-import Label from "../com/form/Label.svelte";
-import FormRow from "../com/form/FormRow.svelte";
+import Title from "../com/card/Title.svelte";
+import Toasts from "../com/Toasts.svelte";
+import WalletStatus from "../com/WalletStatus.svelte";
 
 
 // state mgmt
@@ -28,6 +30,7 @@ const machine = setup( {} ).createMachine( escrowMachine )
 const { snapshot, send } = useMachine( machine, {
     // inspect,
     input: {
+        dbg: false,
         contractAbi: abi,
         contractBytecode: bytecode,
         networkId,
@@ -54,7 +57,6 @@ $:contract = {
     amount
 }
 const emitSetContract = contract => {
-    console.log( 'sent', { contract } )
     send( { type: 'set_contract', contract } )
 }
 const onChangeArbiter = e => {
@@ -69,7 +71,6 @@ const onChangeAmount = e => {
     amount = e.target.value;
     emitSetContract( { ...contract, amount } )
 }
-
 const onReset = () => {
     arbiter = ""
     beneficiary = ""
@@ -77,15 +78,13 @@ const onReset = () => {
     emitSetContract( { arbiter, beneficiary, amount } )
 }
 onMount( () => {
-    // setTimeout( () => { send( { type: 'toast', toast: { type: 'success', message: 'one:success yo yo yo', created_at: performance.now() } } ) }, 1000 )
-    // setTimeout( () => { send( { type: 'toast', toast: { type: 'info', message: 'three:info yo yo yo', created_at: performance.now() } } ) }, 4000 )
-    // setTimeout( () => { send( { type: 'toast', toast: { type: 'error', message: 'two:error yo yo yo', created_at: performance.now() } } ) }, 2000 )
-    // setTimeout( () => { send( { type: 'toast', toast: { type: 'warning', message: 'four:warning yo yo yo', created_at: performance.now() } } ) }, 6000 )
 } )
 </script>
 
-{#if $snapshot.matches( 'loading' )}
-    loading
+{#if $snapshot.matches( 'preparing' )}
+    <div>
+        <span class="loading loading-dots loading-xs"></span>
+    </div>
 {:else}
     <StackedLayout>
 
@@ -104,108 +103,111 @@ onMount( () => {
             </div>
         </header>
 
-        <div class="flex flex-col gap-12">
+        {#if $snapshot.context.error}
+            <Error message={$snapshot.context.error}/>
+        {/if}
 
-            {#if $snapshot.context.error}
-                <Error message={$snapshot.context.error}/>
-            {/if}
+        {#if $snapshot.context.network.available}
+            <Card>
+                <Title>Accounts</Title>
+                <Accounts accounts={$snapshot.context.accounts}/>
+            </Card>
 
-            {#if $snapshot.context.network.available}
+            <Card>
+                <Title>Escrow Contract</Title>
 
-                <Card>
-                    <Accounts accounts={$snapshot.context.accounts}/>
-                </Card>
-
-                <Card>
-
-                    <div class="mb-12">
+                <div class="mb-12">
                     <Steps state={$snapshot.value}/>
-                    </div>
+                </div>
 
-                    <div class="flex flex-col gap-4">
-                        <FormRow>
-                            <Label>Arbiter</Label>
-                            <div class="form-control">
-                                <Select
-                                    options={arbiterOptions}
-                                    selected={$snapshot.context.contract.arbiter}
-                                    on:change={onChangeArbiter}
-                                />
-                            </div>
-                        </FormRow>
-                        <FormRow>
-                            <Label>Beneficiary</Label>
-                            <div class="form-control">
-                                <Select
-                                    options={beneficiaryOptions}
-                                    selected={$snapshot.context.contract.beneficiary}
-                                    on:change={onChangeBenenficiary}
-                                />
-                            </div>
-                        </FormRow>
-                        <FormRow>
-                            <Label>Value</Label>
-                            <div class="form-control">
-                                <input
-                                    type="range"
-                                    name="amount"
-                                    min="0"
-                                    max="10"
-                                    step=".25"
-                                    class="range range-xs w-full ml-3"
-                                    on:change={onChangeAmount}
-                                    bind:value={amount}
-                                />
-                            </div>
-                        </FormRow>
-
-                        <div class="flex gap-2 mt-8 justify-between">
-                            <div>
-                                <button
-                                    disabled={!$snapshot.can({type:'deploy'})}
-                                    on:click={()=>send({type:'deploy'})}
-                                    class="btn btn-warning w-32">
-                                    <span>Deploy</span>
-                                    {#if $snapshot.matches( { running: { ready: 'deploying' } } )}
-                                        <span class="loading loading-spinner loading-xs"></span>
-                                    {/if}
-                                </button>
-                                <button
-                                    disabled={!$snapshot.can({type:'approve'})}
-                                    on:click={()=>send({type:'approve'})}
-                                    class="btn btn-secondary w-32">
-                                    <span>Approve</span>
-                                    {#if $snapshot.matches( { running: { ready: 'approving' } } )}
-                                        <span class="loading loading-spinner loading-xs"></span>
-                                    {/if}
-                                </button>
-                            </div>
-                            <div>
-                                <button
-                                    on:click={onReset}
-                                    class="btn btn-accent">
-                                    Reset
-                                </button>
-                            </div>
+                <div class="flex flex-col gap-4">
+                    <FormRow>
+                        <Label>Arbiter</Label>
+                        <div class="form-control">
+                            <Select
+                                options={arbiterOptions}
+                                selected={$snapshot.context.contract.arbiter}
+                                on:change={onChangeArbiter}
+                            />
                         </div>
-                </Card>
+                    </FormRow>
+                    <FormRow>
+                        <Label>Beneficiary</Label>
+                        <div class="form-control">
+                            <Select
+                                options={beneficiaryOptions}
+                                selected={$snapshot.context.contract.beneficiary}
+                                on:change={onChangeBenenficiary}
+                            />
+                        </div>
+                    </FormRow>
+                    <FormRow>
+                        <Label>Value</Label>
+                        <div class="form-control  flex flex-row items-center gap-x-2">
+                            <input
+                                type="range"
+                                name="amount"
+                                min="0"
+                                max="10"
+                                step=".25"
+                                class="range range-xs w-full ml-3"
+                                on:change={onChangeAmount}
+                                bind:value={amount}
+                            />
+                            <span class="text-sm w-32">{amount} Ether</span>
+                        </div>
+                    </FormRow>
 
-                {#if $snapshot.context.contractAddress}
-                    <div class="flex items-center gap-2">
-                        <span>Deployed to</span>
-                        <code>{$snapshot.context.contractAddress}</code>
+                    <div class="flex gap-4 mt-8 justify-between">
+                        <div>
+                            <button
+                                disabled={!$snapshot.can({type:'deploy'})}
+                                on:click={()=>send({type:'deploy'})}
+                                class="btn btn-warning w-32">
+                                <span>Deploy</span>
+                                {#if $snapshot.matches( { running: { ready: 'deploying' } } )}
+                                    <span class="loading loading-spinner loading-xs"></span>
+                                {/if}
+                            </button>
+                            <button
+                                disabled={!$snapshot.can({type:'approve'})}
+                                on:click={()=>send({type:'approve'})}
+                                class="btn btn-secondary w-32">
+                                <span>Approve</span>
+                                {#if $snapshot.matches( { running: { ready: 'approving' } } )}
+                                    <span class="loading loading-spinner loading-xs"></span>
+                                {/if}
+                            </button>
+                        </div>
+                        <div>
+                            <button
+                                on:click={onReset}
+                                class="btn btn-accent">
+                                Reset
+                            </button>
+                        </div>
                     </div>
-                {/if}
-            {/if}
+                </div>
+            </Card>
 
-        </div>
+            {#if $snapshot.context.deployments.length > 0}
+                <Card>
+                    <Title>Deloyments</Title>
+                    <Deployments deployments={$snapshot.context.deployments}/>
+                </Card>
+            {/if}
+        {/if}
     </StackedLayout>
 {/if}
-<pre>statematch:{$snapshot.matches( { running: { ready: 'valid' } } )}</pre>
-<pre>state:{JSON.stringify( $snapshot.value, null, 4 )}</pre>
-<pre>toasts:{JSON.stringify( $snapshot.context.toasts, null, 4 )}</pre>
-<pre>local:{JSON.stringify( contract, null, 4 )}</pre>
-<pre>error:{JSON.stringify( $snapshot.context, null, 4 )}</pre>
-<pre>error:{JSON.stringify( $snapshot.context.error, null, 4 )}</pre>
-<pre>addre:{JSON.stringify( $snapshot.context.contractAddress, null, 4 )}</pre>
-<!--<pre>{JSON.stringify( $snapshot.value, null, 4 )}</pre>-->
+
+{#if $snapshot.context.dbg}
+    <pre>deployments:{JSON.stringify( $snapshot.context.deployments, null, 4 )}</pre>
+    <!--<pre>statematch:{$snapshot.matches( { running: { ready: 'valid' } } )}</pre>-->
+    <!--<pre>state:{JSON.stringify( $snapshot.value, null, 4 )}</pre>-->
+    <!--<pre>toasts:{JSON.stringify( $snapshot.context.toasts, null, 4 )}</pre>-->
+    <!--<pre>local:{JSON.stringify( contract, null, 4 )}</pre>-->
+    <!--<pre>error:{JSON.stringify( $snapshot.context, null, 4 )}</pre>-->
+    <!--<pre>error:{JSON.stringify( $snapshot.context.error, null, 4 )}</pre>-->
+    <!--<pre>addre:{JSON.stringify( $snapshot.context.contractAddress, null, 4 )}</pre>-->
+    <!--<pre>{JSON.stringify( $snapshot.value, null, 4 )}</pre>-->
+{/if}
