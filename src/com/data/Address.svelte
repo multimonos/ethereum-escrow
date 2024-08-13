49f0007aca14da4308850c5d354358198ebcfbb0
Craig Hopgood <searaig@gmail.com>
2024-08-13 14:46:13 -0400

    shorten address

--- src/com/data/Address.svelte
+++ src/com/data/Address.svelte
@@ -6,14 +6,14 @@ const copy = data => async event => {
     try {
         await navigator.clipboard.writeText( data )
         copied = true
-        setTimeout(()=>copied=false,300)
+        setTimeout( () => copied = false, 300 )
     } catch ( err ) {
         console.log( err )
     }
 }
 </script>
 <div class="flex items-center gap-x-0.5">
-    <span class="font-mono">{address.replace(/^(.{8}).+(.{8})$/,'$1...$2')}</span>
+    <span class="font-mono">{address.replace( /^(.{6}).+(.{4})$/, '$1...$2' )}</span>
     <button class="btn btn-xs btn-ghost" on:click={copy(address)}>
         <span class="icon-[solar--copy-line-duotone]"></span>
     </button>
